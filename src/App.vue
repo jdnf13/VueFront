5373481f2d5e55457009bb1d7c9369b9ea9072ef
jnfranco <jnfranco@jaivana.co>
2022-07-29 11:38:24 -0500

    Se borran archivos no utilizados

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
-
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Vue.js Hello!" />
-
       <nav>
         <RouterLink to="/">Shop</RouterLink>
         <RouterLink to="/about">About</RouterLink>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import Card from '@/components/Card.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Card description="Descripcion del primer producto agregado" 
+        product="Producto 1"
+        price="21.000" />
+    <Card description="Descripcion del segundo producto agregado" 
+        product="Producto 2"
+        price="32.000" />
   </main>
 </template>
